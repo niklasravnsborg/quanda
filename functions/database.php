f24d7f93c5b88d89520766c2bdefc6f38cc27958
Niklas Ravnsborg-Gjertsen <niklas.r.1998@gmail.com>
2014-03-23 09:18:15 +0100

    Changed Charset to UTF-8.

--- functions/database.php
+++ functions/database.php
@@ -4,5 +4,10 @@
     $connid = mysql_pconnect('localhost', 'root', '');
 	if(!$connid) die('CONNECTION ERROR');
 	mysql_select_db('quanda') or die('DATABASE ERROR');
+	
+	mysql_query ('SET NAMES UTF8;');
+	mysql_query ('SET COLLATION_CONNECTION=utf8_general_ci;');
+	mysql_client_encoding($connid);
+	
 	return $connid;
   }
